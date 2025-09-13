fe5a775813f46b8aa5d768aa03f5b9f82efe0724
Zand567 <69153235+Zand567@users.noreply.github.com>
2025-09-13 22:53:09 +0900

    fix refresh token

--- api/axios.ts
+++ api/axios.ts
@@ -17,10 +17,12 @@ export const authAxiosMultipart = axios.create({
 
 // リフレッシュトークンの取得と設定を行うメソッド
 async function refreshAuthLogic(failedRequest: any) {
-    return authAxios.post('refresh_token', {
-      headers: sessionStorage.getItem("refresh_token")
-        ? { Authorization: `Bearer ${sessionStorage.getItem("refresh_token")}` }
-        : {},
+    return authAxios.post("refresh_token/?refresh_token=" + (sessionStorage.getItem("refresh_token") ? sessionStorage.getItem("refresh_token") : ""), {
+    }, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        withCredentials: true
     }).then((tokenRefreshResponse) => {
         sessionStorage.setItem('access_token', tokenRefreshResponse.data.access_token);
         failedRequest.response.config.headers['Authorization'] = 'Bearer ' + tokenRefreshResponse.data.access_token;
