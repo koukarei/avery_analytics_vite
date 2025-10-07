656050793db41ba19c036d7eae33d91c7e8bbbad
koukarei <kareikou.carly@gmail.com>
2025-10-07 14:04:39 +0900

    config fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,8 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       plugins: [react()],
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      server: {
+        host: true,
       },
       resolve: {
         alias: {
